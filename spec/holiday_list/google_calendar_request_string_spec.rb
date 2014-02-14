416482880bb3b5222722765a5007371b162c8778
Ben Hicks <benkhicks@gmail.com>
2014-02-13 21:54:36 -0600

    Cleaning up long lines

--- spec/holiday_list/google_calendar_request_string_spec.rb
+++ spec/holiday_list/google_calendar_request_string_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe HolidayList::GoogleCalendarRequestString do
-  let(:configuration) { double('Configuration', configured?: true).as_null_object }
+  let(:configuration) do
+    double('Configuration', configured?: true).as_null_object
+  end
+
   subject { HolidayList::GoogleCalendarRequestString.new(configuration) }
 
   describe '#new' do
     context 'configured' do
       before do
-        HolidayList::GoogleCalendarRequestString.any_instance.should_not_receive(:argument_error!)
+        HolidayList::GoogleCalendarRequestString
+          .any_instance.should_not_receive(:argument_error!)
       end
 
       it 'creates a new object' do
@@ -18,7 +22,8 @@ describe HolidayList::GoogleCalendarRequestString do
     context 'not configured' do
       before do
         configuration.stub(:configured?).and_return(false)
-        HolidayList::GoogleCalendarRequestString.any_instance.should_receive(:argument_error!)
+        HolidayList::GoogleCalendarRequestString
+          .any_instance.should_receive(:argument_error!)
       end
 
       it 'calls argument_error!' do
